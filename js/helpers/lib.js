e3aff48c23a86bc090da270e48d42628bd6f8c46
abbosxon <abbosxon.oripov@yandex.ru>
2022-05-13 13:58:53 +0500

    add todos

--- js/helpers/lib.js
+++ js/helpers/lib.js
@@ -1,50 +1,50 @@
 export function render(data) {
-  // data.map;
+    // data.map;
 }
 
 export function createElement(id, isDone, text) {
-  let LI = document.createElement("li");
-  LI.dataset.id = id;
-  LI.dataset.isDone = isDone;
-  LI.className = "list-group-item d-flex align-items-center";
+    let LI = document.createElement("li");
+    LI.dataset.id = id;
+    LI.dataset.isDone = isDone;
+    LI.className = "list-group-item d-flex align-items-center";
 
-  let INPUT = document.createElement("input");
-  INPUT.dataset.type = "check";
-  INPUT.className = "form-check-input me-3";
-  INPUT.setAttribute("type", "checkbox");
-  LI.append(INPUT);
+    let INPUT = document.createElement("input");
+    INPUT.dataset.type = "check";
+    INPUT.className = "form-check-input me-3";
+    INPUT.setAttribute("type", "checkbox");
+    LI.append(INPUT);
 
-  let TEXT_DIV = document.createElement("div");
-  TEXT_DIV.className = "text w-100";
-  TEXT_DIV.textContent = text;
-  LI.append(TEXT_DIV);
+    let TEXT_DIV = document.createElement("div");
+    TEXT_DIV.className = "text w-100";
+    TEXT_DIV.textContent = text;
+    LI.append(TEXT_DIV);
 
-  let BTN_WRAPPER = document.createElement("div");
-  BTN_WRAPPER.className = "d-flex gap-1";
+    let BTN_WRAPPER = document.createElement("div");
+    BTN_WRAPPER.className = "d-flex gap-1";
 
-  let EDIT_BTN = document.createElement("button");
-  EDIT_BTN.dataset.type = "edit";
-  EDIT_BTN.className = "btn btn-warning";
-  EDIT_BTN.textContent = "edit";
-  BTN_WRAPPER.append(EDIT_BTN);
+    let EDIT_BTN = document.createElement("button");
+    EDIT_BTN.dataset.type = "edit";
+    EDIT_BTN.className = "btn btn-warning";
+    EDIT_BTN.textContent = "edit";
+    BTN_WRAPPER.append(EDIT_BTN);
 
-  let DELETE_BTN = document.createElement("button");
-  DELETE_BTN.dataset.type = "delete";
-  DELETE_BTN.className = "btn btn-danger";
-  DELETE_BTN.textContent = "delete";
-  BTN_WRAPPER.append(DELETE_BTN);
+    let DELETE_BTN = document.createElement("button");
+    DELETE_BTN.dataset.type = "delete";
+    DELETE_BTN.className = "btn btn-danger";
+    DELETE_BTN.textContent = "delete";
+    BTN_WRAPPER.append(DELETE_BTN);
 
-  LI.append(BTN_WRAPPER);
+    LI.append(BTN_WRAPPER);
 
-  return LI;
+    return LI;
 }
 
 export function todoObjectCreator(text) {
-  return {
-    id: new Date().getTime(),
-    text,
-    isDone: false,
-  };
+    return {
+        id: new Date().getTime(),
+        text,
+        isDone: false,
+    };
 }
 
 export function deleter(id) {}
--- js/main.js
+++ js/main.js
@@ -3,62 +3,78 @@ import * as lib from "./helpers/lib.js";
 
 let storage = window.localStorage;
 let data =
-  JSON.parse(storage.getItem("todos")).length > 0
-    ? JSON.parse(storage.getItem("todos"))
-    : todoArray;
+    JSON.parse(storage.getItem("todos")).length > 0 ?
+    JSON.parse(storage.getItem("todos")) :
+    todoArray;
 
 let makerTodo = document.querySelector(".maker-todo");
 let addTodo = document.querySelector(".add-todo");
 let listGroup = document.querySelector(".list-group");
 
 listGroup.addEventListener("click", (event) => {
-  let pressedNode = event.target;
+    let pressedNode = event.target;
 
-  if (!listGroup.contains(pressedNode)) return;
+    if (!listGroup.contains(pressedNode)) return;
 
-  if (!pressedNode.closest("[data-type]")) return;
+    if (!pressedNode.closest("[data-type]")) return;
 
-  switch (pressedNode.dataset.type) {
-    case "delete":
-      let deleteParentNode = pressedNode.parentNode.parentNode;
-      lib.delete(deleteParentNode.dataset.id);
-      break;
-    case "edit":
-      let editParentNode = pressedNode.parentNode.parentNode;
-      lib.edit(editParentNode.dataset.id);
-      break;
-    case "check":
-      let checkParentNode = pressedNode.parentNode;
-      lib.check(checkParentNode.dataset.id);
-      break;
-  }
+    switch (pressedNode.dataset.type) {
+        case "delete":
+            let deleteParentNode = pressedNode.parentNode.parentNode;
+            // data arraydan tanlangan todoni o'chirish
+            data = data.filter((todo) => {
+                return todo.id != deleteParentNode.dataset.id;
+            });
+
+            // storagega qayta ishlangan massivni saqlash
+            storage.setItem("todos", JSON.stringify(data));
+            // ekrandan LIni o'chirish
+            deleteParentNode.remove();
+            break;
+        case "edit":
+            let editParentNode = pressedNode.parentNode.parentNode;
+            // lib.edit(editParentNode.dataset.id);
+            break;
+        case "check":
+            let checkParentNode = pressedNode.parentNode;
+            // lib.check(checkParentNode.dataset.id);
+            break;
+    }
+
 });
 
 // inputdan listga todoni qo'shish
 addTodo.addEventListener("click", () => {
-  if (makerTodo.value.trim().length <= 3) {
-    alert("belgilar soni 4tadan kam");
-    return;
-  }
+    if (makerTodo.value.trim().length <= 3) {
+        alert("your todo must be longer than 3 elements");
+        return;
+    }
+    if (makerTodo.value.length >= 149) {
+        alert('pls make it shorter than 149 elements');
+        return;
+    }
 
-  let newTodoObject = lib.todoObjectCreator(makerTodo.value);
-  data.push(newTodoObject);
+    let newTodoObject = lib.todoObjectCreator(makerTodo.value);
+    data.push(newTodoObject);
 
-  storage.setItem("todos", JSON.stringify(data));
+    storage.setItem("todos", JSON.stringify(data));
 
-  let newTodoNode = lib.createElement(
-    newTodoObject.id,
-    newTodoObject.isDone,
-    newTodoObject.text
-  );
+    let newTodoNode = lib.createElement(
+        newTodoObject.id,
+        newTodoObject.isDone,
+        newTodoObject.text
+    );
 
-  listGroup.prepend(newTodoNode);
-  makerTodo.value = "";
+    listGroup.prepend(newTodoNode);
+    makerTodo.value = "";
 });
 
 // statik datani listga reverse shaklda render qilish
 data.forEach((todo) => {
-  let newTodoNode = lib.createElement(todo.id, todo.isDone, todo.text);
+    let newTodoNode = lib.createElement(todo.id, todo.isDone, todo.text);
 
-  listGroup.prepend(newTodoNode);
+    listGroup.prepend(newTodoNode);
+
 });
+
+//
